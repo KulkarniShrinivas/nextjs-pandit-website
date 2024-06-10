37fefd57a7fb618b527a197c5f2ea035696764bb
Kunal Arya <kunalaryad@gmail.com>
2024-06-10 08:29:51 +0530

    image slider added in hero section

--- src/app/_components/main.tsx
+++ src/app/_components/main.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useRef } from "react";
 import TextTransition, { presets } from 'react-text-transition';
+import { ImagesSliderDemo } from "./hero-image-slider";
+import { vipPhotosData } from "../../../public/data/photo-gallery-data";
 // import homeImg from "../../assets/home.webp";
 // import left from "../../assets/pLeft.webp";
 // import right from "../../assets/pRight.webp";
@@ -10,43 +12,31 @@ import TextTransition, { presets } from 'react-text-transition';
 // import BookNowBtn from "../book-now-btn/bookNowBtn";
 
 export const mainData = {
-    typeWriterText: ["Authentic Vedic Puja", "In Traditional Way"],
-  };
+  typeWriterText: ["Authentic Vedic Puja", "In Traditional Way"],
+};
 
 const Main = () => {
-    const [index, setIndex] = React.useState(0);
-    const TEXTS = mainData.typeWriterText;
+  const [index, setIndex] = React.useState(0);
+  const TEXTS = mainData.typeWriterText;
 
-    React.useEffect(() => {
-      const intervalId = setInterval(
-        () => setIndex((index) => index + 1),
-        3000, // every 3 seconds
-      );
-      return () => clearTimeout(intervalId);
-    }, []);
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      3000, // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
+  const images = vipPhotosData.map((d) => d.img.src)
 
   return (
-    <section className="main">
-      {/* <div className="half-circle"></div>
-      <div className="half-circle-2"></div> */}
-      {/* <img src={left} className="pattern-left" />
-      <img src={right} className="pattern-right" />
-      <div className="img-div">
-        <Tilt>
-          <img src={homeImg} alt="Ganesh Image" />
-        </Tilt>
-      </div> */}
-
-      <div className="main-headings">
-        <h1>Experience</h1>
-        <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
-      </div>
-      <div className="btn-container">
-        {/* <BookNowBtn onClick={() => whatsappData()} /> */}
-        {/* <CustomButton text={"Book Now"} onClick={() => whatsappData()} /> */}
-      </div>
-      <div className="fade-div"></div>
-    </section>
+    <main className="mt-[70px]">
+      <ImagesSliderDemo
+        images={images}
+        btnName="Book Now"
+        heading={<span>Experience <br/> {mainData.typeWriterText[0]} <br/> {mainData.typeWriterText[1]}</span>}
+      />
+    </main>
   );
 };
 
